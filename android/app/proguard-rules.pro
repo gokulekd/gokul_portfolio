5e55bb5bdadfd2e2a29df7dd7326daa0b9a3f348
Gokul@quicoo <gokul@quicoo.com>
2026-04-21 22:35:29 +0530

    Update Flutter project configuration and clean up ephemeral files. Adjusted application path in environment scripts, removed obsolete files, and updated permissions for generated files. This ensures a cleaner build environment and improved project structure.

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,13 @@
+# Flutter
+-keep class io.flutter.** { *; }
+-keep class io.flutter.plugins.** { *; }
+
+# Firebase
+-keep class com.google.firebase.** { *; }
+-keep class com.google.android.gms.** { *; }
+
+# Google Sign-In
+-keep class com.google.android.gms.auth.** { *; }
+
+# Keep your app's model classes
+-keep class com.example.gokul_portfolio.** { *; }
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gokulks/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gokulks/personal projects/gokul_portfolio/gokul_portfolio"
+export "FLUTTER_APPLICATION_PATH=/Users/gokulks/personal project/gokul_portfolio/gokul_portfolio"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
